34025f1c43f6e26807d47b189304fc9cc48f7d4a
maryana morato <maryana.morato@natahouse.com>
2020-05-31 10:03:06 -0300

    boilerplate ne vei

---
+++ src/components/PartnersForm/index.js
@@ -0,0 +1,12 @@
+import  React  from 'react'
+import './style.css'
+
+const PartersForm = () => {
+    return (
+        <div class='partnersForm'>
+            
+        </div>
+    )
+}
+
+export default PartersForm
