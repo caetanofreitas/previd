efce3ae3e618588b695a2f466700a65d7a010303
caetanofreitas <caetanofreitas72@gmail.com>
2020-05-31 09:26:41 -0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from 'store/store';
+import Routes from 'utils/constants/routes';
 
 const App = () => {
   return (
-    <h1>
-      Hey
-    </h1>
+    <Provider store={store} >
+      <Routes />
+    </Provider>
   );
 }
 
--- src/utils/constants/routes.js
+++ src/utils/constants/routes.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Main from 'pages/Main';
+
 const Routes = () => (
     <BrowserRouter>
         <Switch>
+            <Route exact path="/" component={Main} />
         </Switch>
     </BrowserRouter>
 );
