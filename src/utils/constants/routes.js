30f109dd81d6d027b137f820812973b7830e98ea
Torres <matheustf2003@gmail.com>
2020-05-31 19:07:54 -0300

    challenge

--- src/utils/constants/routes.js
+++ src/utils/constants/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from 'pages/Main';
 import Challenges from 'pages/Challenges';
+import Challenge from 'pages/Challenge'
 import Awards from 'pages/Awards'
 
 const Routes = () => (
@@ -10,6 +11,7 @@ const Routes = () => (
         <Switch>
             <Route exact path="/" component={Main} />
             <Route exact path="/challenges" component={Challenges} />
+            <Route path="/challenges/:id" component={Challenge} />
             <Route exact path="/awards" component={Awards} />
         </Switch>
     </BrowserRouter>
