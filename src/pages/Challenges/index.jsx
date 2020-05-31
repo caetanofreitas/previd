ae12da2c6383efb62b95786db4bb3ccbae8204fe
caetanofreitas <caetanofreitas72@gmail.com>
2020-05-31 16:31:37 -0300

    Starting Challenges Page

--- src/pages/Challenges/index.jsx
+++ src/pages/Challenges/index.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './styles.css';
 import Navbar from 'components/Navbar';
 const ChallengePage = () => {
+    const [isMyChallenge, setIsMyChallenge] = useState(false);
     return(
         <>
             <Navbar />  
+
+            <div className="btnGroup" >
+                <button type="button" className={!isMyChallenge ? 'selected' : ''}>
+                    Challenges
+                </button>
+                <button type="button" className={isMyChallenge ? 'selected' : ''}>
+                    My Challenges
+                </button>
+            </div>  
         </>
     );
 }
